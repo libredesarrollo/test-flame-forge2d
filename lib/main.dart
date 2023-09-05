6f0b823eac665e069040e42f4db34a4baf3f76f0
libredesarrollo <desarrollolibre.net@gmail.com>
2023-09-05 14:39:46 -0400

    varios

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,37 @@
 import 'dart:async';
+import 'dart:math';
 import 'package:flame/flame.dart';
 import 'package:flame/camera.dart' as camera;
 import 'package:flame/components.dart';
 import 'package:flame/events.dart';
 import 'package:flame/game.dart';
 import 'package:flame/palette.dart';
+import 'package:flame/sprite.dart';
 import 'package:flame_forge2d/flame_forge2d.dart';
 import 'package:flutter/material.dart';
+import 'package:testforged2d/utils/create_animation_by_limit.dart';
 
 void main() {
   runApp(GameWidget(game: MyGame()));
 }
 
+// class Explotion extends SpriteAnimationComponent{
+//   @override
+//   FutureOr<void> onLoad() async{
+//     final spriteImage = await Flame.images.load('explotions.png');
+//     final spriteSheet = SpriteSheet(
+//         image: spriteImage,
+//         srcSize: Vector2(192, 192));
+//         animation = spriteSheet.createAnimationByLimit(
+//         xInit: 0, yInit: 0, step: 8, sizeX: 5, stepTime: .08, loop: false);
+//     return super.onLoad();
+//   }
+// }
+
 class MyGame extends Forge2DGame with TapDetector {
   MyGame() : super(gravity: Vector2(0, 30));
   final cameraWorld = camera.World();
   late final CameraComponent cameraComponent;
-
-  final up = Vector2(0, -30);
 
   @override
   Future<void> onLoad() async {
@@ -30,9 +44,15 @@ class MyGame extends Forge2DGame with TapDetector {
   }
 
   @override
-  void onTap() {
-    cameraWorld.add(Box());
-    super.onTap();
+  void onTapDown(TapDownInfo info) {
+    super.onTapDown(info);
+    // cameraWorld.add(Box(info.eventPosition.game));
+
+    if (Random.secure().nextBool()) {
+      cameraWorld.add(Box(info.eventPosition.game));
+    } else {
+      cameraWorld.add(Ball(info.eventPosition.game));
+    }
   }
 }
 
@@ -47,30 +67,108 @@ class BoxComponent extends SpriteComponent {
   }
 }
 
-class Box extends BodyComponent {
+class BallComponent extends SpriteComponent {
+  @override
+  FutureOr<void> onLoad() async {
+    sprite = await Sprite.load('ball.png');
+    size = Vector2.all(10);
+    anchor = Anchor.center;
+
+    return super.onLoad();
+  }
+}
+
+class _Base extends BodyComponent with ContactCallbacks {
+  final Vector2 position;
+  late final SpriteAnimation explotionAnimation;
+
+  _Base(this.position);
+
   @override
   Future<void> onLoad() async {
     await super.onLoad();
-    // paint = BasicPalette.red.paint();
-    paint = BasicPalette.transparent.paint();
+
+    final spriteImage = await Flame.images.load('explotions.png');
+    final spriteSheet =
+        SpriteSheet(image: spriteImage, srcSize: Vector2(192, 192));
+    explotionAnimation = spriteSheet.createAnimationByLimit(
+        xInit: 0, yInit: 0, step: 20, sizeX: 5, stepTime: .03, loop: false);
+  }
+
+  @override
+  void beginContact(Object other, Contact contact) {
+    print("beginContact" + other.toString());
+
+    if (other is Ball || other is Box) {
+      gameRef.add(SpriteAnimationComponent(
+        position: body.position,
+        animation: explotionAnimation.clone(),
+        anchor: Anchor.center,
+        size: Vector2.all(50),
+        removeOnFinish: true,
+      ));
+
+      removeFromParent();
+    }
+
+    super.beginContact(other, contact);
+  }
+
+  @override
+  void endContact(Object other, Contact contact) {
+    print("endContact" + other.toString());
+    super.endContact(other, contact);
+  }
+
+  @override
+  Body createBody() {
+    // TODO: implement createBody
+    throw UnimplementedError();
+  }
+}
+
+class Box extends _Base {
+  Box(super.position);
+
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+
     add(BoxComponent());
-    // final sprite = Sprite(gameRef.images.fromCache('box.png'));
-    // add(
-    //   SpriteComponent(
-    //     sprite: sprite,
-    //     size: Vector2(.5, .5),
-    //     anchor: Anchor.center,
-    //   ),
-    // );
   }
 
   @override
   Body createBody() {
     // Shape shape = CircleShape()..radius = 3;
     final shape = PolygonShape();
-    shape.setAsBoxXY(7, 5);
-    BodyDef bodyDef =
-        BodyDef(position: Vector2(40, 10), type: BodyType.dynamic);
+    shape.setAsBoxXY(5, 5);
+    BodyDef bodyDef = BodyDef(
+        position: Vector2(position.x, position.y),
+        type: BodyType.dynamic,
+        userData: this);
+    FixtureDef fixtureDef =
+        FixtureDef(shape, friction: 1, density: 5, restitution: .1);
+    return world.createBody(bodyDef)..createFixture(fixtureDef);
+  }
+}
+
+class Ball extends _Base {
+  Ball(super.position);
+
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+
+    add(BallComponent());
+  }
+
+  @override
+  Body createBody() {
+    final shape = CircleShape()..radius = 5;
+    BodyDef bodyDef = BodyDef(
+        position: Vector2(position.x, position.y),
+        type: BodyType.dynamic,
+        userData: this);
     FixtureDef fixtureDef =
         FixtureDef(shape, friction: 1, density: 5, restitution: .1);
     return world.createBody(bodyDef)..createFixture(fixtureDef);
@@ -90,6 +188,5 @@ class Ground extends BodyComponent {
 
     FixtureDef fixtureDef = FixtureDef(shape, density: 1, friction: 1);
     return world.createBody(bodyDef)..createFixture(fixtureDef);
-    // throw UnimplementedError();
   }
 }
--- lib/spriteanimation/main.dart
+++ lib/spriteanimation/main.dart
@@ -45,37 +45,3 @@ class MyGame extends Forge2DGame with TapDetector {
     // add(ChopperBody(position, animationComponent));
   }
 }
-
-class ChopperBody extends BodyComponent {
-  final Vector2 position;
-  final Vector2 size;
-
-  ChopperBody(
-    this.position,
-    PositionComponent component,
-  ) : size = component.size {
-    renderBody = false;
-    add(component);
-  }
-
-  @override
-  Body createBody() {
-    final shape = CircleShape()..radius = size.x / 4;
-    final fixtureDef = FixtureDef(
-      shape,
-      userData: this, // To be able to determine object in collision
-      restitution: 0.8,
-      density: 1.0,
-      friction: 0.2,
-    );
-
-    final velocity = (Vector2.random() - Vector2.random()) * 200;
-    final bodyDef = BodyDef(
-      position: position,
-      angle: velocity.angleTo(Vector2(1, 0)),
-      linearVelocity: velocity,
-      type: BodyType.dynamic,
-    );
-    return world.createBody(bodyDef)..createFixture(fixtureDef);
-  }
-}
