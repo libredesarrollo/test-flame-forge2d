2f7b5d6ce776d41d090a158fa6bb7fd8f49fda6d
libredesarrollo <desarrollolibre.net@gmail.com>
2023-09-17 15:31:26 -0400

    varios

---
+++ lib/main_jump.dart
@@ -0,0 +1,41 @@
+import 'dart:async';
+import 'dart:math';
+import 'package:flame/flame.dart';
+import 'package:flame/camera.dart' as camera;
+import 'package:flame/components.dart';
+import 'package:flame/events.dart';
+import 'package:flame/game.dart';
+import 'package:flame_forge2d/flame_forge2d.dart';
+import 'package:flutter/material.dart';
+import 'package:testforged2d/components/player.dart';
+import 'package:testforged2d/test/_main.dart';
+
+void main() {
+  runApp(GameWidget(game: MyGame()));
+}
+
+class MyGame extends Forge2DGame
+    with TapDetector, HasKeyboardHandlerComponents {
+  MyGame() : super(gravity: Vector2(0, 40));
+
+  final cameraWorld = camera.World();
+  late final CameraComponent cameraComponent;
+
+  late PlayerBody playerBody;
+
+  @override
+  Future<void> onLoad() async {
+    cameraComponent = CameraComponent(world: cameraWorld);
+    cameraComponent.viewfinder.anchor = Anchor.topLeft;
+
+    addAll([cameraComponent, cameraWorld]);
+    Vector2 gameSize = screenToWorld(cameraComponent.viewport.size);
+    cameraWorld.add(Ground(gameSize));
+    playerBody = PlayerBody(mapSize: gameSize);
+    cameraWorld.add(playerBody);
+    // playerBody.loaded.then((value) {
+    // print("loaded");
+    // playerBody.body.position = Vector2(20, 10);
+    // });
+  }
+}
